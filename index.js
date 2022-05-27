6d64e6dbcbef41a8875192712ff2f150b33576b5
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-27 15:21:28 -0300

    prettier setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const port = 3000;
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World");
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
 });
 
 app.listen(port, () => {
