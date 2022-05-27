d7fee1b79ab913d8f923136039d3e34df76eca03
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-27 17:32:01 -0300

    add get endpoints

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express();
 
 const port = 3000;
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World');
+const messages = ['First message', 'Second message'];
+
+// - [GET] /messages - Return the message list
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
+// - [GET] /messages/{id} - Return the message by id
+app.get('/messages/:id', (req, res) => {
+    const id = req.params.id - 1;
+    const message = messages[id];
+    res.send(message);
 });
 
 app.listen(port, () => {
-  console.info(`App rodando em http://localhost:${port}`);
+    console.info(`App running on http://localhost:${port}`);
 });
