13858a54fa9838281bc4ee24812a49dc5f8f07f5
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-27 20:04:50 -0300

    change the message list from list of strings to list of objects

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const app = express();
 
 const port = 3000;
 
-const messages = ['First message', 'Second message'];
+const messages = [
+    {
+        id: 1,
+        text: 'First message',
+    },
+    {
+        id: 2,
+        text: 'Second message',
+    },
+];
 
 app.use(bodyParser.json());
 
@@ -16,33 +25,56 @@ app.get('/messages', (req, res) => {
 
 // - [GET] /messages/{id} - Return a message by id
 app.get('/messages/:id', (req, res) => {
-    const id = req.params.id - 1;
-    const message = messages[id];
+    const id = +req.params.id;
+    const message = getMessageById(id);
+    if (!message) {
+        messageNotFound(res);
+        return;
+    }
     res.send(message);
 });
 
 // - [POST] /messages - Create a message
 app.post('/messages', (req, res) => {
-    const message = req.body.message;
+    const message = req.body;
+    const lastMessage = messages[messages.length - 1];
+    if (lastMessage) message.id = lastMessage.id + 1;
+    else message.id = 1;
     messages.push(message);
-    res.send(`Message created: ${message}`);
+    res.send(message);
 });
 
 // - [PUT] /messages/{id} - Update a message by id
 app.put('/messages/:id', (req, res) => {
-    const id = req.params.id - 1;
-    const message = req.body.message;
-    messages[id] = message;
-    res.send(`Message updated: ${message}`);
+    const id = +req.params.id;
+    const messageIndex = getMessageIndexById(id);
+    if (messageIndex === -1) {
+        messageNotFound(res);
+        return;
+    }
+    const message = messages[messageIndex];
+    message.text = req.body.text;
+    messages[messageIndex] = message;
+    res.send(message);
 });
 
 // - [DELETE] /messages/{id} - Remove a message by id
 app.delete('/messages/:id', (req, res) => {
-    const id = req.params.id - 1;
-    const message = messages[id];
-    delete messages[id];
-    res.send(`Message deleted: ${message}`);
+    const id = +req.params.id;
+    const messageIndex = getMessageIndexById(id);
+    if (messageIndex === -1) {
+        messageNotFound(res);
+        return;
+    }
+    const message = messages.splice(messageIndex, 1);
+    res.send(message[0]);
 });
+
+const getMessageById = id => messages.find(message => message.id === id);
+const getMessageIndexById = id =>
+    messages.findIndex(message => message.id === id);
+
+const messageNotFound = res => res.send('Message not found.');
 
 app.listen(port, () => {
     console.info(`App running on http://localhost:${port}`);
